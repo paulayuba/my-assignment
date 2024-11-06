ade22039c146f5adb24546fdfaab254a5ad9d4ca
paulayuba <paulayuba788@gmail.com>
2024-11-06 20:53:31 +0100

    update in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,32 @@
 import Image from "next/image";
 import HeaderFooter from "@/components/layout/layout"
 import {backgroundIMage} from "../../public/icons/index"
-import CustomBackgroundButton from "@/components/common/CustomBackgroundButton";
 export default function Home() {
   return (
     <HeaderFooter>
       <div className="w-full h-full flex flex-col ">
         
         <div className="w-full h-fit flex relative bg-white">
-          <div className="w-[20%] h-full absolute z-[3] bg-[#535353]">
+          <div className="w-[20%] h-full absolute z-[3]">
 
           </div>
-          <div className="w-full px-36 flex gap-10 flex-col justify-center h-full absolute z-[4] bg-black/85">
-              <h3 className="w-fit text-[60px] leading-[86px] font-semibold text-white">Empowering Your Business with Cutting- <br /><span className="flex justify-center">Edge Solutions</span></h3>
-               
-               <CustomBackgroundButton text={"Sign Up for Free"}  buttonName={"!rounded-[4px] "}/>
+          <div className="w-full px-36 flex gap-10 flex-col justify-center h-full absolute z-[4] bg-[#172755]/85">
+              <h3 className="w-fit text-[60px] leading-[48px] font-semibold text-white mt-10">Empowering Your Business with Cutting- <br /><span className="flex justify-center mt-8">Edge Solutions</span></h3>
+              <p className="text-xl items-center text-white px-72 mt-8">Join us in transforming the way you operate for better efficiency and growth.</p>
+              <div className="flex flex-col md:flex-row justify-center items-center gap-4">
+               <button className="border border-gray-800 py-4 w-96 px-9 rounded-md focus:outline-none">
+                Enter Your Email Address
+              </button>
+              <button className="bg-[#EF2AB2] text-white py-4 px-6 rounded-md focus:outline-none">
+              Get Started Today
+              </button>
+          </div>
           </div>
           <div className="h-[703px] w-full object-cover" style={{
-            background: `url(${backgroundIMage.src})`,
-            backgroundRepeat: "no-repeat",
-            backgroundPosition: "center",
-            objectFit: "cover"
           }}>
 
           </div>
         </div>
-        
       </div>
     </HeaderFooter>
   );
--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -37,8 +37,21 @@ export default function Navbar(){
     ]
     return(
         <div className=" w-full h-[158px] px-20 fixed z-[10] flex items-center justify-between bg-transparent">
-            <div className="w-fit flex flex-row items-center h-fit">
-                <p className="text-white text-4xl px-1 border-red-100"><RiMenu2Line /></p>
+            <div className="w-fit flex flex-row items-center h-fit gap-2">
+                <p className="text-white text-4xl px-1 border-red-100 bg-purple-400 border rounded-full"><svg width="41" height="40" viewBox="0 0 41 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <rect x="0.5" width="40" height="40" rx="20" fill="url(#paint0_linear_1_20)"/>
+                    <rect x="9.30011" y="6.40039" width="22.4" height="6" fill="white"/>
+                    <rect x="4.10004" y="17.2012" width="22.4" height="6" fill="white"/>
+                    <rect x="9.30011" y="28" width="22.4" height="6" fill="white"/>
+                    <defs>
+                    <linearGradient id="paint0_linear_1_20" x1="20.5" y1="0" x2="20.5" y2="40" gradientUnits="userSpaceOnUse">
+                    <stop offset="0.09375" stop-color="#455EB5"/>
+                    <stop offset="0.484375" stop-color="#5643CC"/>
+                    <stop offset="0.692708" stop-color="#673FD7"/>
+                    </linearGradient>
+                    </defs>
+                    </svg>
+                    </p>
                 <h3 className="text-[30px] text-white leading-[42px] font-semibold">Innovative Solutions Group</h3>
             </div>
              
